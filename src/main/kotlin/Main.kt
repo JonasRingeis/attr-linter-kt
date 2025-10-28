320c91805baadbff485140ee4d9cb66075331577
JonasRingeis <jonas.ringeis@otto.de>
2025-10-28 17:00:01 +0100

    feat: add test print of env vars

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,6 +16,7 @@ class Cli : CliktCommand() {
     val exitOnViolation: Boolean by option().boolean().default(true).help("Exit with code 1 when finding any violations")
 
     override fun run() {
+        println(System.getenv().entries.joinToString("\n") { "${it.key}: ${it.value}" })
         Linter(this).lint()
     }
 }
