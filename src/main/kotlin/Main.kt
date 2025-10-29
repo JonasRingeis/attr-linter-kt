82844d41e72f1b0be8790c92e1994e8e8c362d80
JonasRingeis <jonas.ringeis@otto.de>
2025-10-29 16:26:55 +0100

    feat: add ability to read args from properties file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,27 @@
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.boolean
+import com.github.ajalt.clikt.sources.PropertiesValueSource
 
 class Cli : CliktCommand() {
+    init {
+        context {
+            valueSource = PropertiesValueSource.from("attr-linter.properties")
+        }
+    }
+
     val targetPath: String by option().default("./").help("The path the linter is checking")
 
     val projectAlias: String by option().required().help("Project alias used in 'data-fti-xx-' attributes")
 
     val ignoreHidden: Boolean by option().boolean().default(true).help("Ignore all files and directories that are hidden")
     val forbidDataId: Boolean by option().boolean().default(true).help("Forbid the use of the data-id attribute")
+    val forbidId: Boolean by option().boolean().default(true).help("Forbid the use of the data-id attribute")
     val exitOnViolation: Boolean by option().boolean().default(true).help("Exit with code 1 when finding any violations")
 
     override fun run() {
